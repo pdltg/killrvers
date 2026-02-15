e53dc1df4ceef983a26e1eca37da81e52ea1f236
pdltg <pdlovestogame@gmail.com>
2026-02-15 21:13:49 +0000

    Check 'world' before initializing physics for 'sing'
    Added a check for 'world' before initializing physics components.

--- testgame.lua
+++ testgame.lua
@@ -8,9 +8,13 @@ local sing = {
 }
 
 function game.load()
-    sing.body = love.physics.newBody(world, sing.x, sing.y, "static")
-    sing.shape = love.physics.newRectangleShape(sing.w, sing.h)
-    sing.fixture = love.physics.newFixture(sing.body, sing.shape)
+    if world == nil then
+        print("weird bug. worked before like this and now it refuses to work. if this debug checker is not appearing. then love2d is broken beyond repair")
+    else
+        sing.body = love.physics.newBody(world, sing.x, sing.y, "static")
+        sing.shape = love.physics.newRectangleShape(sing.w, sing.h)
+        sing.fixture = love.physics.newFixture(sing.body, sing.shape)
+    end
 end
 
 function game.loop(dt)
