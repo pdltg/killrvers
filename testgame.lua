680462d429f2ae8f367eaf72f17611a1b57c7707
pdltg <pdlovestogame@gmail.com>
2026-03-09 18:51:44 +0000

    Add kill object and update game logic

--- testgame.lua
+++ testgame.lua
@@ -2,9 +2,13 @@ local game = {done = false,name = "Example Game",id = 0}
 
 -- game code
 
-hello()
-
 local sing = {
+    x = 50,
+    y = 100,
+    w = 1000,
+    h = 20
+}
+local kill = {
     x = 50,
     y = 100,
     w = 1000,
@@ -19,14 +23,23 @@ function game.load()
         sing.shape = love.physics.newRectangleShape(sing.w, sing.h)
         sing.fixture = love.physics.newFixture(sing.body, sing.shape)
     end
+    kill.body = love.physics.newBody(world, kill.x, kill.y, "static")
+    kill.shape = love.physics.newRectangleShape(kill.w, kill.h)
+    kill.fixture = love.physics.newFixture(kill.body, kill.shape)
+    -- do something with this curently it damages the player when they spawn
+    HurtPlayer(10)
 end
 
 function game.loop(dt)
     sing.x,sing.y = sing.body:getPosition()
+    kill.x,kill.y = kill.body:getPosition()
 end
 
 function game.draw()
+    love.graphics.setColor(1,1,1)
     love.graphics.rectangle("fill", sing.x - sing.w/2, sing.y - sing.h/2, sing.w, sing.h)
+    love.graphics.setColor(1,0,0)
+    love.graphics.rectangle("fill", 70, 90, kill.w, kill.h)
 end
 
 function game.drawui()
