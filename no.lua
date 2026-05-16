6ff1e626e2064db8374475f2fe62e1fb2fa5e10f
Pdltg <pdlovestogame@gmail.com>
2026-05-16 23:04:38 +0100

    Add 'done' property to game object
    Added 'done' property to game object and set it to true.

--- no.lua
+++ no.lua
@@ -1,3 +1,21 @@
-local game = {name = "Game Deleted", id = 404, }
+local game = {name = "Game Deleted", id = 404, done = false}
+
+function game.load()
+  
+end
+
+function game.loop()
+
+end
+
+function game.draw()
+
+end
+
+function game.drawui()
+  love.graphics.print("This Game Was Deleted.", 0, 100)
+end
+    
+game.done = true
 
 return game
