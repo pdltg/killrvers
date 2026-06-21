e8cb133337016516f22f72f3ecd1112596afdda0
Pdltg <pdlovestogame@gmail.com>
2026-06-21 18:48:02 +0100

    Initialize game module with basic functions

---
+++ uhh.lua
@@ -0,0 +1,18 @@
+local game = {}
+
+function game.load()
+end
+
+function game.loop(dt)
+
+end
+
+function game.draw()
+  love.graphics.rectangle("fill", 50, 50, 500, 10)
+end
+
+function game.drawui()
+
+end
+
+return game
