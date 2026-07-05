10c0af42d58d521d3bbf6a2b1faff731fd993905
Pdltg <pdlovestogame@gmail.com>
2026-07-05 17:15:18 +0100

    Initialize game structure in killy_quest.lua

--- killy_quest.lua
+++ killy_quest.lua
@@ -1,1 +1,23 @@
+local game = {done = false,name = "ARDUINO KLUED QUEST",id = 0}
 
+-- game code
+
+function game.load()
+    
+end
+
+function game.loop(dt)
+    
+end
+
+function game.draw()
+    
+end
+
+function game.drawui()
+    
+end
+
+game.done = true
+
+return game
