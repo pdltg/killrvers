404c8b56c4c0caffbeda1c7fc8dae5674bd4e51b
Pdltg <pdlovestogame@gmail.com>
2026-07-05 17:18:56 +0100

    Modify game ID and implement serial port communication
    Updated game ID and added serial communication setup.

--- killy_quest.lua
+++ killy_quest.lua
@@ -1,4 +1,26 @@
-local game = {done = false,name = "ARDUINO KLUED QUEST",id = 0}
+local game = {done = false,name = "ARDUINO KLUED QUEST",id = 3}
+
+local port = "/dev/ttyACM0"
+local baud = 9600
+
+os.execute(string.format("stty -F %s %d raw -echo", port, baud))
+
+
+local serial, err = io.open(port, "w")
+
+if serial then
+    
+    serial:setvbuf("no") 
+    
+
+    serial:write("Hello World\n")
+    serial:flush()
+    
+
+    serial:close()
+else
+    print("Error opening port: " .. tostring(err))
+end
 
 -- game code
 
