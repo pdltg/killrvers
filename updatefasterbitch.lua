5891a7a7333b0846a2f259ed752df6187219396e
Pdltg <pdlovestogame@gmail.com>
2026-09-22 18:17:50 +0100

    Refactor game logic and add client display functions

--- updatefasterbitch.lua
+++ updatefasterbitch.lua
@@ -1,14 +1,65 @@
-local game = {}
+game = {}
 
-function game.load()
+game.physics = [[
+local playerSpeed = 220
+local gravity = 900
+local jumpPower = 420
+
+function init()
+    createNewObject(0, 550, 900, 50, "anchored")
+    createNewObject(300, 450, 100, 100, "unanchored")
 end
 
-function game.loop(dt)
+function input(player, input)
+    player.vx = input.x * playerSpeed
 
+    if input.jump and player.grounded then
+        player.vy = -jumpPower
+        player.grounded = false
+    end
 end
 
-function game.draw()
-  love.graphics.rectangle("fill", 50, 50, 500, 10)
+function step(dt)
+    for _, player in pairs(getPlayers()) do
+        player.vy = player.vy + gravity * dt
+
+        player.x = player.x + player.vx * dt
+        player.y = player.y + player.vy * dt
+
+        if player.y + player.h / 2 >= 550 then
+            player.y = 550 - player.h / 2
+            player.vy = 0
+            player.grounded = true
+        end
+    end
+
+    for _, object in pairs(getObjects()) do
+        if not object.anchored then
+            object.vy = object.vy + gravity * dt
+            object.y = object.y + object.vy * dt
+
+            if object.y + object.h / 2 >= 550 then
+                object.y = 550 - object.h / 2
+                object.vy = 0
+            end
+        end
+    end
+end
+]]
+
+game.client = [[
+function init()
 end
 
-return game
+function update(dt)
+end
+
+function draw()
+    love.graphics.setColor(0.2, 0.8, 0.3)
+    love.graphics.print("MULTIPLAYER TEST", 20, 20)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("WASD / Arrow Keys = Move", 20, 45)
+    love.graphics.print("Space = Jump", 20, 65)
+end
+]]
